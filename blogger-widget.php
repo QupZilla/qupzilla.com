59ae653a63fb0824bffe6567964e8831c5647a71
David Rosca <nowrep@gmail.com>
2014-04-08 22:36:27 +0200

    [Blogger Widget] Load links in new tab (taget="_blank")

--- blogger-widget.php
+++ blogger-widget.php
@@ -14,18 +14,18 @@ require_once('app/bootstrap.php');
 </head>
 <body style="background: #e5e5e5; width:320px; margin:3px; padding:0;">
 <div style="background: #e5e5e5; font-size:85%;">
-<!-- Blogger widget -->
+
 <script type="text/javascript" src="http://www.google.com/jsapi?key=ABQIAAAAq3SVCR-K0yI5_N0cswX1txT0VykOMYx3Zz_NUtzmY0vQvYY8dRRumpjgNGF4yD2y0xdghBjp7upN8A"></script>
 <script type="text/javascript">
 google.load("feeds", "1");
 function OnLoad() {
    var feedControl = new google.feeds.FeedControl();
    feedControl.addFeed("http://blog.qupzilla.com/feeds/posts/default?alt=rss", "");
+   feedControl.setLinkTarget("_blank");
    feedControl.draw(document.getElementById("feedControl"));
 }
 google.setOnLoadCallback(OnLoad);
 </script>
-<!-- Blogger widget -->
 
 <span id="feedControl"><?php echo $LANG["feed_loading"]; ?></span>
 </div>
