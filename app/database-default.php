3fff89c5b8905d0811efcdc04ced7dc6e4e341a9
David Rosca <nowrep@gmail.com>
2014-04-10 09:47:35 +0200

    [Database] Show error and die when connection to database fails

--- app/database-default.php
+++ app/database-default.php
@@ -3,5 +3,8 @@ define("DB_HOST", 'localhost');
 define("DB_USER", '');
 define("DB_PASS", '');
 define("DB_NAME", '');
+
 $con = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+if (!$con)
+    die("Cannot connect do database!");
 ?>
