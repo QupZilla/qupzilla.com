b11dc89185143676e2c37492fa35b665f555f4f9
David Rosca <nowrep@gmail.com>
2014-12-13 21:04:58 +0100

    Update links for 1.8.5 release

--- app/common.php
+++ app/common.php
@@ -1,7 +1,7 @@
 <?php
 // Globals
-$qupzilla_version = "1.8.4";
-$qupzilla_version_date = "03.11.2014";
+$qupzilla_version = "1.8.5";
+$qupzilla_version_date = "12.12.2014";
 
 // Functions
 function getRealIp() {
