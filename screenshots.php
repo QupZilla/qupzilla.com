cbd843359bb443e479f6b7f6586889a911815b5d
David Rosca <nowrep@gmail.com>
2015-10-16 11:48:30 +0200

    Rename screenshots to have constant names

--- screenshots.php
+++ screenshots.php
@@ -8,7 +8,8 @@
       <img width="200" height="200" src="screens/thumbs/kde_window.png"></a>
       </td>
       <td>
-      <a title="<?php echo $LANG["screens_speeddial"]; ?>: Linux KDE" href="screens/kde_speeddial.png" rel="lightbox[screens]"><img width="200" height="200" src="screens/thumbs/kde_speeddial.png"></a>
+      <a title="<?php echo $LANG["screens_speeddial"]; ?>: Linux KDE" href="screens/kde_speeddial.png" rel="lightbox[screens]">
+      <img width="200" height="200" src="screens/thumbs/kde_speeddial.png"></a>
       </td>
     <td><a title="<?php echo $LANG["screens_library"]; ?>: Linux KDE" href="screens/kde_library.png" rel="lightbox[screens]">
     <img width="200" height="200" src="screens/thumbs/kde_library.png"></a></td>
@@ -17,44 +18,47 @@
     </td></tr>
     <tr><td colspan=4><h2>Linux Gnome</h2></td></tr>
       <tr><td>
-      <a title="<?php echo $LANG["qupzilla"]; ?>: Linux Gnome" href="screens/gnome.png" rel="lightbox[screens]">
-      <img width="200" height="200" src="screens/thumbs/gnome.png"></a>
+      <a title="<?php echo $LANG["qupzilla"]; ?>: Linux Gnome" href="screens/gnome_window.png" rel="lightbox[screens]">
+      <img width="200" height="200" src="screens/thumbs/gnome_window.png"></a>
       </td>
       <td>
-      <a title="<?php echo $LANG["screens_speeddial"]; ?>: Linux Gnome" href="screens/dial_gnome.png" rel="lightbox[screens]"><img width="200" height="200" src="screens/thumbs/dial_gnome.png"></a>
+      <a title="<?php echo $LANG["screens_speeddial"]; ?>: Linux Gnome" href="screens/gnome_speeddial.png" rel="lightbox[screens]">
+      <img width="200" height="200" src="screens/thumbs/gnome_speeddial.png"></a>
       </td>
-    <td><a title="<?php echo $LANG["screens_library"]; ?>: Linux Gnome" href="screens/library_gnome.png" rel="lightbox[screens]">
-    <img width="200" height="200" src="screens/thumbs/library_gnome.png"></a></td>
-    <td><a title="<?php echo $LANG["screens_preferences"]; ?>: Linux Gnome" href="screens/preferences_gnome.png" rel="lightbox[screens]">
-    <img width="200" height="200" src="screens/thumbs/preferences_gnome.png"></a>
+    <td><a title="<?php echo $LANG["screens_library"]; ?>: Linux Gnome" href="screens/gnome_library.png" rel="lightbox[screens]">
+    <img width="200" height="200" src="screens/thumbs/gnome_library.png"></a></td>
+    <td><a title="<?php echo $LANG["screens_preferences"]; ?>: Linux Gnome" href="screens/gnome_preferences.png" rel="lightbox[screens]">
+    <img width="200" height="200" src="screens/thumbs/gnome_preferences.png"></a>
     </td></tr>
 
     <tr><td colspan=4><h2>Windows Vista/7</h2></td></tr>
     <tr><td>
-      <a title="<?php echo $LANG["qupzilla"]; ?>: Windows Vista/7" href="screens/win7.png" rel="lightbox[screens]">
-      <img width="200" height="200" src="screens/thumbs/win7.png"></a>
+      <a title="<?php echo $LANG["qupzilla"]; ?>: Windows Vista/7" href="screens/win7_window.png" rel="lightbox[screens]">
+      <img width="200" height="200" src="screens/thumbs/win7_window.png"></a>
       </td>
       <td>
-      <a title="<?php echo $LANG["screens_speeddial"]; ?>: Windows Vista/7" href="screens/dial_win7.png" rel="lightbox[screens]"><img width="200" height="200" src="screens/thumbs/dial_win7.png"></a>
+      <a title="<?php echo $LANG["screens_speeddial"]; ?>: Windows Vista/7" href="screens/win7_speeddial.png" rel="lightbox[screens]">
+      <img width="200" height="200" src="screens/thumbs/win7_speeddial.png"></a>
       </td>
-    <td><a title="<?php echo $LANG["screens_library"]; ?>: Windows Vista/7" href="screens/library_win7.png" rel="lightbox[screens]">
-    <img width="200" height="200" src="screens/thumbs/library_win7.png"></a></td>
-    <td><a title="<?php echo $LANG["screens_preferences"]; ?>: Windows Vista/7" href="screens/preferences_win7.png" rel="lightbox[screens]">
-    <img width="200" height="200" src="screens/thumbs/preferences_win7.png"></a>
+    <td><a title="<?php echo $LANG["screens_library"]; ?>: Windows Vista/7" href="screens/win7_library.png" rel="lightbox[screens]">
+    <img width="200" height="200" src="screens/thumbs/win7_library.png"></a></td>
+    <td><a title="<?php echo $LANG["screens_preferences"]; ?>: Windows Vista/7" href="screens/win7_preferences.png" rel="lightbox[screens]">
+    <img width="200" height="200" src="screens/thumbs/win7_preferences.png"></a>
     </td></tr>
 
     <tr><td colspan=4><h2>Windows XP</h2></td></tr>
     <tr><td>
-      <a title="<?php echo $LANG["qupzilla"]; ?>: Windows XP" href="screens/winxp.png" rel="lightbox[screens]">
-      <img width="200" height="200" src="screens/thumbs/winxp.png"></a>
+      <a title="<?php echo $LANG["qupzilla"]; ?>: Windows XP" href="screens/winxp_window.png" rel="lightbox[screens]">
+      <img width="200" height="200" src="screens/thumbs/winxp_window.png"></a>
       </td>
       <td>
-      <a title="<?php echo $LANG["screens_speeddial"]; ?>: Windows XP" href="screens/dial_winxp.png" rel="lightbox[screens]"><img width="200" height="200" src="screens/thumbs/dial_winxp.png"></a>
+      <a title="<?php echo $LANG["screens_speeddial"]; ?>: Windows XP" href="screens/winxp_speeddial.png" rel="lightbox[screens]">
+      <img width="200" height="200" src="screens/thumbs/winxp_speeddial.png"></a>
       </td>
-    <td><a title="<?php echo $LANG["screens_history"]; ?>: Windows XP" href="screens/library_winxp.png" rel="lightbox[screens]">
-    <img width="200" height="200" src="screens/thumbs/library_winxp.png"></a></td>
-    <td><a title="<?php echo $LANG["screens_preferences"]; ?>: Windows XP" href="screens/preferences_winxp.png" rel="lightbox[screens]">
-    <img width="200" height="200" src="screens/thumbs/preferences_winxp.png"></a>
+    <td><a title="<?php echo $LANG["screens_history"]; ?>: Windows XP" href="screens/winxp_library.png" rel="lightbox[screens]">
+    <img width="200" height="200" src="screens/thumbs/winxp_library.png"></a></td>
+    <td><a title="<?php echo $LANG["screens_preferences"]; ?>: Windows XP" href="screens/winxp_preferences.png" rel="lightbox[screens]">
+    <img width="200" height="200" src="screens/thumbs/winxp_preferences.png"></a>
     </td></tr>
 
     <tr><td colspan=4><h2><?php echo $LANG["screens_other"]; ?></h2></td></tr>
