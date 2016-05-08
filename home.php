cbd22bb690402fd7713145c8c71d3f8b74eb7502
Isaiah Patton <isaiahpatton1@gmail.com>
2016-05-08 14:59:18 -0400

    Change code, easyer to read and edit

--- home.php
+++ home.php
@@ -1,3 +1,60 @@
+<!DOCTYPE html>
+<html lang="en_US">
+<head>
+  <meta http-equiv="content-type" content="text/html; charset=utf-8">
+  <meta name="description" content="QupZilla is a lightweight multiplatform web browser written in Qt Framework and using its web rendering engine QtWebEngine.">
+  <meta name="keywords" content="qupzilla, web browser, qt, qtwebengine, lightweight, multiplatform, crossplatform">
+  <meta name="author" content="David Rosca">
+  <meta name="robots" content="index, follow">
+  <meta name="viewport" content="width=device-width,initial-scale=1">
+  <link rel="stylesheet" href="css/reset.css" type="text/css" media="all">
+  <link rel="stylesheet" href="css/grid.css" type="text/css" media="all">
+  <link rel="stylesheet" href="css/style.css" type="text/css" media="all">
+  <link rel="stylesheet" href="css/responsive.css" type="text/css" media="all">
+  <link rel="stylesheet" href="css/jquery.cointipper.min.css" type="text/css" media="all">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/lightbox2/2.7.1/css/lightbox.css" type="text/css" media="screen">
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/lightbox2/2.7.1/js/lightbox.min.js"></script>
+  <script src="js/jquery.cointipper-pack.min.js"></script>
+  <title>QupZilla - Lightweight multiplatform browser</title>
+  <!--[if lt IE 7]>
+  	<link rel="stylesheet" href="css/ie/ie6.css" type="text/css" media="screen">
+    <script type="text/javascript" src="js/ie/ie_png.js"></script>
+    <script type="text/javascript">
+        ie_png.fix('.png, h3 img, .button span');
+    </script>
+  <![endif]-->
+  <!--[if lt IE 9]>
+  	<script type="text/javascript" src="js/ie/html5.js"></script>
+  <![endif]-->
+    <link rel="stylesheet" href="css/lang/en_US.css" type="text/css" media="all">
+</head>
+<body>
+            <a target="_blank" href="https://github.com/QupZilla/"><img class="forkme" style="position: absolute; top: 0; left: auto; right: 0; border: 0;" src="images/forkme.png" alt="Fork me on GitHub"></a>
+    
+<!-- header -->
+  <header>
+    <div class="container_24">
+	<!-- .logo -->
+	<div id="logbase">
+    	<div class="logo">
+      	<h1><a href="/">QupZilla</a></h1>
+		</div>
+	  </div>
+    	<!-- /.logo -->
+      <nav>
+        <ul>
+          <li><a href="home" class="current" >Home</a></li>
+          <li><a href="about"  >About</a></li>
+          <li><a href="screenshots"  >Screenshots</a></li>
+          <li><a href="download"  >Download</a></li>
+          <li><a href="https://github.com/QupZilla/qupzilla/wiki/FAQ" target="blank">FAQ</a></li>
+          <li><a href="contribute"   >Contribute</a></li>
+          <li><a href="https://bugaos.weebly.com">The default browser of Buga OS</a></li>
+        </ul>
+      </nav>
+    </div>
+  </header>
   <section id="content">
 	<div class="container_24">
 	<div class="top-block">
@@ -6,8 +63,8 @@
 	 </div>
 	 <div class="top-column-right">
 	 <div id="download-box">
-	 <div class="download-text"><?php echo $LANG["header_description"]; ?></div>
-	 <a href="download" class="download-button"><?php echo $LANG["actual_version_button"]; ?></a> <span class="download-version"><?php echo $qupzilla_version; ?></span>
+	 <div class="download-text">Web Browser</div>
+	 <a href="download" class="download-button">DOWNLOAD</a> <span class="download-version">2.0.0</span>
 	 </div>
 	 </div>
 	</div>
@@ -22,30 +79,30 @@
      	       <div class="grid_15">
                <div class="suffix_1">
 
-              <h4><?php echo $LANG["looknfeel_header"]; ?></h4>
-              <span class="pindex"><?php echo $LANG["looknfeel_text"]; ?></span>
+              <h4>Native look'n'feel</h4>
+              <span class="pindex">QupZilla is using native widgets style on major Linux Desktop Environments. It is also using icons from the active desktop icon theme. If you find native themes too boring or have some problems with it, you can always switch to other themes.</span>
 
-              <h4><?php echo $LANG["library_header"]; ?></h4>
-              <span class="pindex"><?php echo $LANG["library_text"]; ?></span>
+              <h4>Unified Library</h4>
+              <span class="pindex">QupZilla unifies bookmarks, history and rss reader in one well-arranged window. No more multiple windows, QupZilla uses just one!<br/> With the integrated rss reader, you can stay up to date with your favourite sites. QupZilla can also import bookmarks from other browsers.</span>
 
-              <h4><?php echo $LANG["adblock_header"]; ?></h4>
-              <span class="pindex"><?php echo $LANG["adblock_text"]; ?></span>
+              <h4>Integrated AdBlock</h4>
+              <span class="pindex">Are you bored of websites full of advertisements? Are they eating your bandwidth and time? The only thing you need with QupZilla is to update EasyList or maybe add your own rules and start browsing ad free.</span>
 
-              <h4><?php echo $LANG["speeddial_header"]; ?></h4>
-              <span class="pindex"><?php echo $LANG["speeddial_text"]; ?></span>
+              <h4>Speed Dial</h4>
+              <span class="pindex">This popular extension is finally available for QupZilla users! You can now access your favourite pages as fast as you want on one page opened in new tab. Needless to say that it fully supports drag&amp;drop and page thumbnail loading.</span>
 
             </div>
           </div>
         	<div class="grid_9">
           	<!-- .box1 -->
           	<div class="box1">
-                    <h2 style="margin-bottom:0px;"><?php echo $LANG["devnews_header"]; ?></h2>
+                    <h2 style="margin-bottom:0px;">Development News</h2>
                     <iframe src="blogger-widget.php" style="width:330px; height:400px;" scrolling="no"> </iframe>
                 </div>
 
         <div style="margin: 30px 0px 15px 30px;">
-         <a style="margin-right:5px;" href="contribute#donate" class="download-button"><?php echo $LANG["donate"]; ?></a>
-         <span style="font-size:18px;"><?php echo $LANG["dev_donatehelp"]; ?></span>
+         <a style="margin-right:5px;" href="contribute#donate" class="download-button">Donate</a>
+         <span style="font-size:18px;">to help with development</span>
         </div>
           	<!-- /.box1 -->
           </div>
@@ -53,3 +110,67 @@
       </div>
     </div>
   </section>
+<!-- footer -->
+  <footer>
+    <div class="container_24">
+        <iframe src="https://tylerlh.github.io/github-latest-commits-widget/?username=QupZilla&repo=qupzilla&limit=5"
+        allowtransparency="true" frameborder="0" scrolling="no" id="gh-frame"></iframe>
+        <nav>
+        <ul>
+        <li><a href="/">Home</a>|</li>
+        <li><a href="/about">About</a>|</li>
+        <li><a href="/download">Download</a>|</li>
+        <li><a href="https://github.com/QupZilla/qupzilla/wiki/FAQ" target="blank">FAQ</a>|</li>
+        <li><a href="/contribute">Contribute</a></li>
+        </ul>
+        </nav>
+      &copy; 2010 - 2015 David Rosca and the QupZilla Community      <p>Laptop icon designed by the Oxygen Project</p>
+      <p> </p>
+      Available page translations:     <div class="languages">
+      <nav>
+        <ul>
+        <li><a href="?lang=ar">العربية</a></li>
+        <li><a href="?lang=es">Castellano</a></li>
+        <li><a href="?lang=ca">Català</a></li>
+        <li><a href="?lang=cs">Čeština</a></li>
+        <li><a href="?lang=de">Deutsch</a></li>
+        <li><a href="?lang=en" class="english">English</a></li>
+        <li><a href="?lang=el">Ελληνικά</a></li>
+        <li><a href="?lang=uk">Українська</a></li>
+        </ul>
+        <ul>
+        <li><a href="?lang=fa">پارسی</a></li>
+        <li><a href="?lang=fr">Français</a></li>
+        <li><a href="?lang=gl">Galego</a></li>
+        <li><a href="?lang=he">עברית</a></li>
+        <li><a href="?lang=hr">Hrvatski</a></li>
+        <li><a href="?lang=it">Italiano</a></li>
+        <li><a href="?lang=lt">Lietuvių</a></li>
+        <li><a href="?lang=sk">Slovenčina</a></li>
+        </ul>
+        <ul>
+        <li><a href="?lang=ka">ქართული</a></li>
+        <li><a href="?lang=nl">Nederlands</a></li>
+        <li><a href="?lang=nqo">ߒߞߏ</a></li>
+        <li><a href="?lang=ja">日本語</a></li>
+        <li><a href="?lang=pl">Polski</a></li>
+        <li><a href="?lang=pt">Português</a></li>
+        <li><a href="?lang=pt-br">Português (Brazil)</a></li>
+        <li><a href="?lang=ru">Pусский</a></li>
+        </ul>
+        <ul>
+        <li><a href="?lang=sr">српски (екавски)&lrm;</a></li>
+        <li><a href="?lang=sr-ba">српски (ијекавски)&lrm;</a></li>
+        <li><a href="?lang=id">Bahasa Indonesia</a></li>
+        <li><a href="?lang=lv">Latviešu valoda</a></li>
+        <li><a href="?lang=fi">Suomi</a></li>
+        <li><a href="?lang=tr">Türkçe</a></li>
+        <li><a href="?lang=zh-cn">简体中文</a></li>
+        <li><a href="?lang=zh-tw">繁体中文</a></li>
+        </ul>
+      </nav>
+     </div>
+    </div>
+</footer>
+</body>
+</html>
