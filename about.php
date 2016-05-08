815084036b331516f8f0b0decd366e5591dd9212
Isaiah Patton <isaiahpatton1@gmail.com>
2016-05-08 15:10:21 -0400

    ALL CODE IS NOW EASYER TO READ AND EDIT

--- about.php
+++ about.php
@@ -1,21 +1,79 @@
+<!DOCTYPE html>
+<html lang="en_US">
+<head>
+  <meta http-equiv="content-type" content="text/html; charset=utf-8">
+  <meta name="description" content="QupZilla is a lightweight multiplatform web browser written in Qt Framework and using its web rendering engine QtWebEngine.">
+  <meta name="keywords" content="qupzilla, web browser, qt, qtwebengine, lightweight, multiplatform, crossplatform">
+  <meta name="author" content="David Rosca">
+  <meta name="robots" content="index, follow">
+  <meta name="viewport" content="width=device-width,initial-scale=1">
+  <link rel="stylesheet" href="css/reset.css" type="text/css" media="all">
+  <link rel="stylesheet" href="css/grid.css" type="text/css" media="all">
+  <link rel="stylesheet" href="css/style.css" type="text/css" media="all">
+  <link rel="stylesheet" href="css/responsive.css" type="text/css" media="all">
+  <link rel="stylesheet" href="css/jquery.cointipper.min.css" type="text/css" media="all">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/lightbox2/2.7.1/css/lightbox.css" type="text/css" media="screen">
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/lightbox2/2.7.1/js/lightbox.min.js"></script>
+  <script src="js/jquery.cointipper-pack.min.js"></script>
+  <title>QupZilla - Lightweight multiplatform browser</title>
+  <!--[if lt IE 7]>
+  	<link rel="stylesheet" href="css/ie/ie6.css" type="text/css" media="screen">
+    <script type="text/javascript" src="js/ie/ie_png.js"></script>
+    <script type="text/javascript">
+        ie_png.fix('.png, h3 img, .button span');
+    </script>
+  <![endif]-->
+  <!--[if lt IE 9]>
+  	<script type="text/javascript" src="js/ie/html5.js"></script>
+  <![endif]-->
+    <link rel="stylesheet" href="css/lang/en_US.css" type="text/css" media="all">
+</head>
+<body>
+            <a target="_blank" href="https://github.com/QupZilla/"><img class="forkme" style="position: absolute; top: 0; left: auto; right: 0; border: 0;" src="images/forkme.png" alt="Fork me on GitHub"></a>
+    
+<!-- header -->
+  <header>
+    <div class="container_24">
+	<!-- .logo -->
+	<div id="logbase">
+    	<div class="logo">
+      	<h1><a href="/">QupZilla</a></h1>
+		</div>
+	  </div>
+    	<!-- /.logo -->
+      <nav>
+        <ul>
+          <li><a href="home"  >Home</a></li>
+          <li><a href="about" class="current" >About</a></li>
+          <li><a href="screenshots"  >Screenshots</a></li>
+          <li><a href="download"  >Download</a></li>
+          <li><a href="https://github.com/QupZilla/qupzilla/wiki/FAQ" target="blank">FAQ</a></li>
+          <li><a href="contribute"  >Contribute</a></li>
+        </ul>
+      </nav>
+    </div>
+  </header>
  <section id="content">
   	<div class="container_24">
      <div class="row-1 row">
       	<div class="wrapper">
 
-          	<h2><?php echo $LANG["about"]; ?></h2>
-            <p><?php echo $LANG["about_text"]; ?> </p>
+          	<h2>About QupZilla</h2>
+            <p>QupZilla is a new and very fast QtWebEngine browser. It aims to be a lightweight web browser available through all major platforms. This project has been originally started only for educational purposes. But from its start, QupZilla has grown into a feature-rich browser.<p>
+QupZilla has all standard functions you expect from a web browser. It includes bookmarks, history (both also in sidebar) and tabs. Above that, it has by default enabled blocking ads with a built-in AdBlock plugin. </p>
 
-            <h4><?php echo $LANG["history"]; ?></h4>
-            <p><?php echo $LANG["history_text"]; ?></p>
+            <h4>History</h4>
+            <p>The very first version of QupZilla has been released in December 2010 and it was written in Python with PyQt4 bindings. After a few versions, QupZilla has been completely rewritten in C++ with the Qt Framework. The Windows version of QupZilla was compiled using MingW, but due to a huge problem with Flash, it is now compiled with Microsoft Visual C++ Compiler 2008. First public release was 1.0.0-b4.<p>
+Until version 2.0, QupZilla was using QtWebKit. QtWebKit is now deprecated and new versions are using QtWebEngine.</p>
 
-            <h4><?php echo $LANG["creators"]; ?></h4>
+            <h4>Who creates QupZilla?</h4>
             <p>
              <img style="width:140px;height:140px;padding:10px;float:left;" src="images/davidrosca.jpg">
              <p>
              <h5 style="padding-top:10px;">David Rosca (nowrep)</h5>
-             <?php echo $LANG["creatortext1"]; ?> <br/>
-             <?php echo $LANG["creatortext2"]; ?> <br/>
+             Project owner and main developer <br/>
+             Student at FIT CTU <br/>
              <div style="height:10px;"></div>
              <b>IRC:</b> nowrep (irc.freenode.net) <br/>
              <b>E-mail:</b> nowrep at gmail dot com <br/>
@@ -23,7 +81,7 @@
              </p>
             </p>
             <p style="clear:both;"></p>
-            <p><?php echo $LANG["creators_text"]; ?></p>
+            <p>Apart from coding, others are contributing also by making translations or supporting QupZilla. Full list of contributors can be found <a href='https://github.com/QupZilla/qupzilla/blob/master/AUTHORS'>here</a> [at github].<br/>You can also join IRC channel <b>#qupzilla</b> at irc.freenode.net to chat with people involved in QupZilla.</p>
             </p>
 
           </div>
@@ -33,3 +91,67 @@
       </div>
     </div>
   </section>
+<!-- footer -->
+  <footer>
+    <div class="container_24">
+        <iframe src="https://tylerlh.github.io/github-latest-commits-widget/?username=QupZilla&repo=qupzilla&limit=5"
+        allowtransparency="true" frameborder="0" scrolling="no" id="gh-frame"></iframe>
+        <nav>
+        <ul>
+        <li><a href="/">Home</a>|</li>
+        <li><a href="/about">About</a>|</li>
+        <li><a href="/download">Download</a>|</li>
+        <li><a href="https://github.com/QupZilla/qupzilla/wiki/FAQ" target="blank">FAQ</a>|</li>
+        <li><a href="/contribute">Contribute</a></li>
+        </ul>
+        </nav>
+      (c) 2010 - 2015 David Rosca and the QupZilla Community      <p>Laptop icon designed by the Oxygen Project</p>
+      <p> </p>
+      Available page translations:     <div class="languages">
+      <nav>
+        <ul>
+        <li><a href="?lang=ar">العربية</a></li>
+        <li><a href="?lang=es">Castellano</a></li>
+        <li><a href="?lang=ca">Català</a></li>
+        <li><a href="?lang=cs">Čeština</a></li>
+        <li><a href="?lang=de">Deutsch</a></li>
+        <li><a href="?lang=en" class="english">English</a></li>
+        <li><a href="?lang=el">Ελληνικά</a></li>
+        <li><a href="?lang=uk">Українська</a></li>
+        </ul>
+        <ul>
+        <li><a href="?lang=fa">پارسی</a></li>
+        <li><a href="?lang=fr">Français</a></li>
+        <li><a href="?lang=gl">Galego</a></li>
+        <li><a href="?lang=he">עברית</a></li>
+        <li><a href="?lang=hr">Hrvatski</a></li>
+        <li><a href="?lang=it">Italiano</a></li>
+        <li><a href="?lang=lt">Lietuvių</a></li>
+        <li><a href="?lang=sk">Slovenčina</a></li>
+        </ul>
+        <ul>
+        <li><a href="?lang=ka">ქართული</a></li>
+        <li><a href="?lang=nl">Nederlands</a></li>
+        <li><a href="?lang=nqo">ߒߞߏ</a></li>
+        <li><a href="?lang=ja">日本語</a></li>
+        <li><a href="?lang=pl">Polski</a></li>
+        <li><a href="?lang=pt">Português</a></li>
+        <li><a href="?lang=pt-br">Português (Brazil)</a></li>
+        <li><a href="?lang=ru">Pусский</a></li>
+        </ul>
+        <ul>
+        <li><a href="?lang=sr">српски (екавски)&lrm;</a></li>
+        <li><a href="?lang=sr-ba">српски (ијекавски)&lrm;</a></li>
+        <li><a href="?lang=id">Bahasa Indonesia</a></li>
+        <li><a href="?lang=lv">Latviešu valoda</a></li>
+        <li><a href="?lang=fi">Suomi</a></li>
+        <li><a href="?lang=tr">Türkçe</a></li>
+        <li><a href="?lang=zh-cn">简体中文</a></li>
+        <li><a href="?lang=zh-tw">繁体中文</a></li>
+        </ul>
+      </nav>
+     </div>
+    </div>
+</footer>
+</body>
+</html>
