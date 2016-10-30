e0cb2daab9caaae43c9442e98b87e110c915ead9
hakking <pandacar@gmail.com>
2016-10-30 23:17:24 +0800

    Update translations.php

--- app/translations.php
+++ app/translations.php
@@ -18,7 +18,7 @@ if (isset($_GET['lang']) && $_GET["lang"] != "") {
 $ACCEPT_LANGUAGE = strtolower($ACCEPT_LANGUAGE);
 
 $short_lang = substr($ACCEPT_LANGUAGE, 0, 2);
-$ISO639_3_lang = substr($ACCEPT_LANGUAGE, 0, 3);
+$iso639_3_lang = substr($ACCEPT_LANGUAGE, 0, 3);
 $long_lang = substr($ACCEPT_LANGUAGE, 0, 5);
 
 // Default language
@@ -175,7 +175,7 @@ switch ($short_lang) {
         break;
 }
 
-switch ($ISO639_3_lang) {
+switch ($iso639_3_lang) {
      case "nqo":
         $lang_id = "nqo";
         $lang_direction = "RTL";
