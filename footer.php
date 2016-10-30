32c539979c666da76b85d10c46a22580d6725aa9
hakking <pandacar@gmail.com>
2016-10-30 22:21:34 +0800

    Update footer.php
    Add languages

--- footer.php
+++ footer.php
@@ -20,44 +20,47 @@
       <nav>
         <ul>
         <li><a href="?lang=ar">العربية</a></li>
-        <li><a href="?lang=es">Castellano</a></li>
-        <li><a href="?lang=ca">Català</a></li>
-        <li><a href="?lang=cs">Čeština</a></li>
-        <li><a href="?lang=de">Deutsch</a></li>
-        <li><a href="?lang=en" class="english">English</a></li>
-        <li><a href="?lang=el">Ελληνικά</a></li>
-        <li><a href="?lang=uk">Українська</a></li>
-        <li><a href="?lang=fa">پارسی</a></li>
-        </ul>
-        <ul>
-        <li><a href="?lang=fr">Français</a></li>
+        <li><a href="?lang=zh-tw">正體中文</a></li>
+        <li><a href="?lang=nl">Nederlands</a></li>
         <li><a href="?lang=gl">Galego</a></li>
         <li><a href="?lang=he">עברית</a></li>
-        <li><a href="?lang=hr">Hrvatski</a></li>
         <li><a href="?lang=it">Italiano</a></li>
         <li><a href="?lang=lt">Lietuvių</a></li>
-        <li><a href="?lang=sk">Slovenčina</a></li>
-        <li><a href="?lang=ka">ქართული</a></li>
-        <li><a href="?lang=nl">Nederlands</a></li>
-        </ul>
-        <ul>
-        <li><a href="?lang=nqo">ߒߞߏ</a></li>
-        <li><a href="?lang=ja">日本語</a></li>
-        <li><a href="?lang=pl">Polski</a></li>
         <li><a href="?lang=pt">Português</a></li>
-        <li><a href="?lang=ru">Pусский</a></li>
-        <li><a href="?lang=is">Íslenska</a></li>
-        <li><a href="?lang=sr">српски (екавски)&lrm;</a></li>
-        <li><a href="?lang=sr-ba">српски (ијекавски)&lrm;</a></li>
-        <li><a href="?lang=id">Bahasa Indonesia</a></li>
+        <li><a href="?lang=sk">Slovenčina</a></li>
         </ul>
         <ul>
-        <li><a href="?lang=lv">Latviešu valoda</a></li>
+        <li><a href="?lang=eu">Euskara</a></li>
+        <li><a href="?lang=hr">Hrvatski</a></li>
+        <li><a href="?lang=en" class="english">English</a></li>
+        <li><a href="?lang=ka">ქართული</a></li>
+        <li><a href="?lang=zh-hk">香港正體字</a></li>
+        <li><a href="?lang=ja">日本語</a></li>
+        <li><a href="?lang=nqo">ߒߞߏ</a></li>
+        <li><a href="?lang=ru">Pусский</a></li>
+        <li><a href="?lang=es">Castellano</a></li>
+        </ul>
+        <ul>
+        <li><a href="?lang=ca">Català</a></li>
+        <li><a href="?lang=cs">Čeština</a></li>
         <li><a href="?lang=fi">Suomi</a></li>
+        <li><a href="?lang=de">Deutsch</a></li>
+        <li><a href="?lang=is">Íslenska</a></li>
+        <li><a href="?lang=ko">한국어</a></li>
+        <li><a href="?lang=fa">پارسی</a></li>
+        <li><a href="?lang=sr">српски (екавски)&lrm;</a></li>
         <li><a href="?lang=tr">Türkçe</a></li>
+        </ul>
+        <ul>
         <li><a href="?lang=zh-cn">中文</a></li>
-        <li><a href="?lang=zh-hk">香港正體字</a></li>  
-        <li><a href="?lang=zh-tw">正體中文</a></li>
+        <li><a href="?lang=da">Dansk</a></li>
+        <li><a href="?lang=fr">Français</a></li>
+        <li><a href="?lang=el">Ελληνικά</a></li>
+        <li><a href="?lang=id">Bahasa Indonesia</a></li>
+        <li><a href="?lang=lv">Latviešu valoda</a></li>
+        <li><a href="?lang=pl">Polski</a></li>
+        <li><a href="?lang=sr-ba">српски (ијекавски)&lrm;</a></li>
+        <li><a href="?lang=uk">Українська</a></li>
         </ul>
       </nav>
      </div>
