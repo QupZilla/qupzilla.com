657f1fd070bd9695ed34836b298eecac9ea5ae1d
David Rosca <nowrep@gmail.com>
2018-01-16 18:20:04 +0100

    2.2.4 release

--- app/common.php
+++ app/common.php
@@ -1,10 +1,10 @@
 <?php
 // Globals
-$qupzilla_version = "2.2.3";
+$qupzilla_version = "2.2.4";
 
 // Files
 $qupzilla_downloads = [
-    "appimage" => "/uploads/QupZilla-2.2.3.AppImage",
+    "appimage" => "/uploads/QupZilla-2.2.4.AppImage",
     "mac" => "/uploads/QupZilla-2.2.3.dmg",
     "os2" => "/uploads/qupzilla-1.8.9-os2.7z"
 ];
